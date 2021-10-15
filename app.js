acd71a7bf654b638a61e60f4a12ec17116a2c7af
Steven Watson <swatson.emu@gmail.com>
2021-10-15 16:13:41 -0500

    Test

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const anchor = document.getElementById("anchor");
 const body = document.getElementById("body");
 
+const sound = new Audio("chris-gilbert-laugh.mp3");
 // let clickEvent = new Event("click");
 // clickEvent.isTrusted = true;
 
-let sound = null;
-anchor.addEventListener("markerFound", (e) => {
-  sound = new Audio("chris-gilbert-laugh.mp3");
+body.addEventListener("click", (e) => {
+  sound.play();
+});
 
+anchor.addEventListener("markerFound", (e) => {
   sound.play();
   console.log("played sound");
   anchor.addEventListener("markerLost", (e) => {
