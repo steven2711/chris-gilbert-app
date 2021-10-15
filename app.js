548be8fedc8ae9af996733441c62a3c0c2e98e38
Steven Watson <swatson.emu@gmail.com>
2021-10-15 16:47:32 -0500

    ios test

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+Element.prototype._addEventListener = Element.prototype.addEventListener;
+Element.prototype.addEventListener = function () {
+  let args = [...arguments];
+  let temp = args[1];
+  args[1] = function () {
+    let args2 = [...arguments];
+    args2[0] = Object.assign({}, args2[0]);
+    args2[0].isTrusted = true;
+    return temp(...args2);
+  };
+  return this._addEventListener(...args);
+};
+
 const anchor = document.getElementById("anchor");
 const body = document.getElementById("body");
 const ios = document.getElementById("ios");
@@ -5,17 +18,21 @@ const ios = document.getElementById("ios");
 const sound = new Audio("chris-gilbert-laugh.mp3");
 let clickEvent = new Event("click");
 
-ios.addEventListener("click", (e) => {
-  sound.play();
-});
+// ios.addEventListener("click", (e) => {
+//   sound.play();
+// });
 
 anchor.addEventListener("markerFound", (e) => {
-  // sound.play();
+  console.log(e.isTrusted);
+
+  sound.play();
   // console.log("played sound");
   // anchor.addEventListener("markerLost", (e) => {
   //   sound.pause();
   //   console.log("paused sound");
   // });
-
-  ios.dispatchEvent(clickEvent);
+  // ios.dispatchEvent(clickEvent);
 });
+
+// ios user event workaround
+// https://stackoverflow.com/questions/49518959/javascript-trigger-an-inputevent-istrusted-true
