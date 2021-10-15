8007551f96b3e2d91e1bea5e1a81048e2b02ea91
Steven Watson <swatson.emu@gmail.com>
2021-10-15 17:10:50 -0500

    test

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-Element.prototype._addEventListener = Element.prototype.addEventListener;
-Element.prototype.addEventListener = function () {
-  let args = [...arguments];
-  let temp = args[1];
-  args[1] = function () {
-    let args2 = [...arguments];
-    args2[0] = Object.assign({}, args2[0]);
-    args2[0].isTrusted = true;
-    return temp(...args2);
-  };
-  return this._addEventListener(...args);
-};
+// Element.prototype._addEventListener = Element.prototype.addEventListener;
+// Element.prototype.addEventListener = function () {
+//   let args = [...arguments];
+//   let temp = args[1];
+//   args[1] = function () {
+//     let args2 = [...arguments];
+//     args2[0] = Object.assign({}, args2[0]);
+//     args2[0].isTrusted = true;
+//     return temp(...args2);
+//   };
+//   return this._addEventListener(...args);
+// };
 
 const anchor = document.getElementById("anchor");
 const body = document.getElementById("body");
@@ -19,15 +19,12 @@ let sound = null;
 let clickEvent = new Event("click");
 
 body.addEventListener("click", (e) => {
-  console.log(e.isTrusted);
   sound = new Audio("chris-gilbert-laugh.mp3");
-  sound.play();
 });
 
 anchor.addEventListener("markerFound", (e) => {
-  console.log(e.isTrusted);
   // sound = new Audio("chris-gilbert-laugh.mp3");
-  // sound.play();
+  sound.play();
 
   // sound.play();
   // console.log("played sound");
@@ -35,7 +32,7 @@ anchor.addEventListener("markerFound", (e) => {
   //   sound.pause();
   //   console.log("paused sound");
   // });
-  ios.dispatchEvent(clickEvent);
+  // ios.dispatchEvent(clickEvent);
 });
 
 // ios user event workaround
