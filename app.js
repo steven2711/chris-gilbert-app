66f09b56a92b6b687a91cb30ca7138fff4d4c837
Steven Watson <swatson.emu@gmail.com>
2021-10-15 16:10:39 -0500

    Test

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const anchor = document.getElementById("anchor");
-const test = document.getElementById("scene");
-const sound = new Audio("chris-gilbert-laugh.mp3");
+const body = document.getElementById("body");
 
+// let clickEvent = new Event("click");
+// clickEvent.isTrusted = true;
+
+let sound = null;
 anchor.addEventListener("markerFound", (e) => {
-  // sound.play();
-  // console.log("played sound");
-  // anchor.addEventListener("markerLost", (e) => {
-  //   sound.pause();
-  //   console.log("paused sound");
-  // });
+  sound = new Audio("chris-gilbert-laugh.mp3");
 
-  test.style.display = "none";
+  sound.play();
+  console.log("played sound");
+  anchor.addEventListener("markerLost", (e) => {
+    sound.pause();
+    console.log("paused sound");
+  });
 });
