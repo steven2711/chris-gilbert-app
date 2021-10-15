38e0ffe880a3e126bd3ad767ef32e09f52e8be51
Steven Watson <swatson.emu@gmail.com>
2021-10-15 14:45:29 -0500

    Test for ios

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const anchor = document.getElementById("anchor");
+const sound = new Audio("chris-gilbert-laugh.mp3");
 
 anchor.addEventListener("markerFound", (e) => {
-  const sound = new Audio("chris-gilbert-laugh.mp3");
   sound.play();
-
   console.log("played sound");
 
   // anchor.addEventListener("markerLost", (e) => {
